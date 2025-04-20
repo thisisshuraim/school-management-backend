d6f3018faef206bfb4f5f86e6f87e0138d586ae6
Shuraim <thisisshuraim@gmail.com>
2025-04-20 19:14:37 +0530

    Populated user data in the students.js file

--- routes/students.js
+++ routes/students.js
@@ -5,21 +5,39 @@ const Marksheet = require('../models/Marksheet');
 const Timetable = require('../models/Timetable');
 const { protect, restrictTo } = require('../middleware/auth');
 const router = express.Router();
+
 router.use(protect);
-router.get('/', restrictTo('admin'), async (req, res) => res.json(await Student.find()));
-router.post('/', restrictTo('admin'), async (req, res) => res.status(201).json(await Student.create(req.body)));
-router.put('/:id', restrictTo('admin'), async (req, res) => res.json(await Student.findByIdAndUpdate(req.params.id, req.body, { new: true })));
-router.delete('/:id', restrictTo('admin'), async (req, res) => res.json(await Student.findByIdAndDelete(req.params.id)));
+
+// ✅ Populated user field here
+router.get('/', restrictTo('admin'), async (req, res) =>
+  res.json(await Student.find().populate('user'))
+);
+
+router.post('/', restrictTo('admin'), async (req, res) =>
+  res.status(201).json(await Student.create(req.body))
+);
+
+router.put('/:id', restrictTo('admin'), async (req, res) =>
+  res.json(await Student.findByIdAndUpdate(req.params.id, req.body, { new: true }))
+);
+
+router.delete('/:id', restrictTo('admin'), async (req, res) =>
+  res.json(await Student.findByIdAndDelete(req.params.id))
+);
+
 router.get('/timetable', restrictTo('student'), async (req, res) => {
   const s = await Student.findOne({ user: req.user.id });
   res.json(await Timetable.find({ classSection: s.classSection }));
 });
+
 router.get('/assignments', restrictTo('student'), async (req, res) => {
   const s = await Student.findOne({ user: req.user.id });
   res.json(await Assignment.find({ classSection: s.classSection }));
 });
+
 router.get('/marksheets', restrictTo('student'), async (req, res) => {
   const s = await Student.findOne({ user: req.user.id });
   res.json(await Marksheet.find({ student: s._id }));
 });
+
 module.exports = router;
