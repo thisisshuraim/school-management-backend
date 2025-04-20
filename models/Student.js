b7d992166d5f50e35383f7bb75c7757a8554bb35
Shuraim <thisisshuraim@gmail.com>
2025-04-20 19:17:46 +0530

    Added name field to Student model

--- models/Student.js
+++ models/Student.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const studentSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  classSection: { type: String, required: true }
+  classSection: { type: String, required: true },
+  name: { type: String, required: true }
 });
 module.exports = mongoose.model('Student', studentSchema);
