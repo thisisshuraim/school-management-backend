eaa4e67c055ab18efa481efeacfc4c59663770d2
Shuraim <thisisshuraim@gmail.com>
2025-04-21 12:38:07 +0530

    Updated s3 region

--- utils/s3.js
+++ utils/s3.js
@@ -2,7 +2,7 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-  region: 'us-south-1',
+  region: 'ap-south-1',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
