2ef56314869040bf95adc3be509ee7f56909eb1e
Shuraim <thisisshuraim@gmail.com>
2025-04-22 02:56:49 +0530

    EC2 setup

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ app.use('/api/timetable', require('./routes/timetable'));
 app.use('/api/lectures', require('./routes/lectures'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
