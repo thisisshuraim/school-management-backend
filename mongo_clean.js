897e3cec066c40c5ca06d8369af64e141dc3818f
Shuraim <thisisshuraim@gmail.com>
2025-04-23 13:46:09 +0530

    Added mongo clean script

---
+++ mongo_clean.js
@@ -0,0 +1,38 @@
+// mongo_seed_script.js
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
+const User = require('./models/User');
+const Student = require('./models/Student');
+const Teacher = require('./models/Teacher');
+const Assignment = require('./models/Assignment');
+const Lecture = require('./models/Lecture');
+const Marksheet = require('./models/Marksheet');
+const Timetable = require('./models/Timetable');
+const Announcement = require('./models/Announcement');
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/school-management';
+
+async function cleanData() {
+  await mongoose.connect(MONGO_URI);
+  console.log('Connected to DB');
+
+  await User.deleteMany();
+  await Student.deleteMany();
+  await Teacher.deleteMany();
+  await Assignment.deleteMany();
+  await Lecture.deleteMany();
+  await Marksheet.deleteMany();
+  await Timetable.deleteMany();
+  await Announcement.deleteMany();
+
+  console.log('Cleaning complete');
+  process.exit();
+}
+
+cleanData().catch(err => {
+  console.error('Cleaning error:', err);
+  process.exit(1);
+});
