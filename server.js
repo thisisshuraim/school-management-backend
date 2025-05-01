c953ff3101b32c17ea3bac3af8364bbe452d2de9
Shuraim <thisisshuraim@gmail.com>
2025-05-01 16:39:16 +0530

    Fixed bug

--- server.js
+++ server.js
@@ -37,4 +37,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
