a151b1665aeee7c8bef71d9988fbb0d03e036a2b
Shuraim <thisisshuraim@gmail.com>
2025-08-30 21:35:01 +0530

    Added log

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username : username?.toLowerCase() });
+  console.log(username, password, user);
   if (user && user?.isBlocked) return res.status(401).json({ message: 'User is blocked' });
   if (!user || !bcrypt.compareSync(password, user.password)) return res.status(401).json({ message: 'Invalid credentials' });
   const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '7d' });
